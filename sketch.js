9e747bb92ecb715d35a009a814286caf52aa7a21
Low fat lard <94632232+Low-Fat-Lard@users.noreply.github.com>
2022-02-09 16:06:54 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -58,7 +58,7 @@ function keyPressed() {
 }
 
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
+    createCanvas(1280, 720);
     for (let i = 0; i < population; i++) {
         players[i] = new Player(generatePlayerX(), height - 64);
     }
