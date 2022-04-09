d93a8fa2d6bc81be4b80d52091433b99b9c92897
Low fat lard <94632232+Low-Fat-Lard@users.noreply.github.com>
2022-04-09 08:04:00 +0100

    Update sketch.js
    added hearts, one time collision and rickroll

--- sketch.js
+++ sketch.js
@@ -1,42 +1,38 @@
 const playerWidth = 32;
 const playerHeight = 32;
 
+var population = 250;
 var players = [];
 var obstaclePositions = [544, 576, 608, 640, 672, 704, 736];
 var obstacles = [];
 const maxNumberOfObstacles = 8;
-
+var lives = 1;
+var displayLives = [];
 var trees = [];
 const maxNumberOfTrees = 25;
 
 var alivePlayers = 0;
 var bestScore = 0;
 var inteligence = true;
-var population = 250;
+var debugLives = 0;
 var uiFont;
-var leftPressed = false;
-var rightPressed = false;
 var uiFontSize = 32;
 var simpleGraphics = false;
 var counter = 0;
 var runSpeed = 1;
 var grass1, grass2;     // two grass objects are needed for the infinite grass effect
-
-// for loading existing player
-const filename = "bestplayer.json";
-let brainJSON = {};
+var leftPressed = false;
+var rightPressed = false;
 var activePlayer = null;
 
 function preload() {
     uiFont = "monospace";
-    brainJSON = loadJSON(filename);
 }
 
 function generatePlayerX() {
     let playerX = obstaclePositions[Math.floor(random(0, obstaclePositions.length))];
     return playerX;
 }
-
 function saveBest() {       // saves the best player to file
     if (bestPlayerSoFar) {
         if (bestPlayerSoFar.score > getBestPlayer().score)
@@ -52,11 +48,7 @@ function loadBest(x, y) {   // loads the best player from file
 }
 
 function keyPressed() {
-    if (key == 'S') {
-        saveBest();
-    } else if (key == 'L') {        
-        loadBest(generatePlayerX(), height - 64);
-    }
+    
 }
 document.addEventListener('keydown', logKey);
 document.addEventListener('keyup', logKey_up);
@@ -93,21 +85,32 @@ function setup() {
 function showUI() {
     fill(255);    
     textAlign(LEFT);
-
-    if (!activePlayer) {
-        text("generation: " + generation, 10, 28);
-        text("best score: " + bestScore, 10, 64);
-        text("remaining: " + alivePlayers + "/" + population, 10, 100);
-        text("AI: " + inteligence, 10, 136);
+    if (!activePlayer && inteligence == true) {
+        text("generation: " + generation, 10, 38);
+        text("score: " + bestScore, 10, 74);
+    
+        text("remaining: " + alivePlayers + "/" + population, 10, 110);
+    } else if(!activePlayer && inteligence == false){
+        text("lives: " + displayLives.join(""), 10, 38);
+        text("score: " + bestScore, 10, 74);
+        text("tick: " + debugLives, 10, 110);
     } else {
-        text("score: " + activePlayer.score, 10, 28);
+        text("score: " + activePlayer.score, 10, 38);
     }
 }
-
+function drawHearts(){
+    displayLives = []
+    for(var i = 0; i < debugLives;i++){
+        displayLives[displayLives.length] = "❤️";
+    }
+}
 function draw() { 
+    
     for (let n = 0; n < runSpeed; n++) {
+        
         if (counter % 25 == 0) {     // create new obstacles
-            let randIndex = Math.floor(random(0, obstaclePositions.length));
+            let randIndex = Math.floor(random(0, obstaclePositions.length));      
+            
             obstacles.push(new Obstacle(obstaclePositions[randIndex], -75));
             trees.push(new Tree(400, -150));
             trees.push(new Tree(800, -150));
@@ -154,8 +157,11 @@ function draw() {
                     bestScore = players[i].score;
             }
 
-            if (alivePlayers == 0)
+            if (alivePlayers == 0 && inteligence == true){
                 nextGeneration();
+            }if(alivePlayers == 0 && inteligence == false){
+                nextmanualGeneration();
+            }
         } else {
             activePlayer.update();
         }
