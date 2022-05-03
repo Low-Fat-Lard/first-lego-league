d4a79477e42a1f921ab0c3b2c2cd2814f65ce8ca
Low fat lard <94632232+Low-Fat-Lard@users.noreply.github.com>
2022-05-03 17:01:41 +0100

    Update gameObjects.js

--- gameObjects.js
+++ gameObjects.js
@@ -139,3 +139,71 @@ class Player {
         }
     }
 }
+class Obstacle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.velX = 0;
+        this.velY = 5;   
+        this.width = playerWidth;
+        this.height = playerHeight;
+        this.col = color(255, 0, 0); 
+    }
+
+    collision(thing) {
+        if (thing.x + thing.width > this.x && thing.x < this.x + this.width &&
+            thing.y + thing.height > this.y && thing.y < this.y + this.height)
+            return true;        
+        else
+            return false;            
+    }
+
+    update() {
+        this.x += this.velX;
+        this.y += this.velY;
+    }
+
+    show() {   
+        if (simpleGraphics) {     
+            stroke(0);
+            fill(this.col);
+            rect(this.x, this.y, this.width, this.height);
+        } else {
+            textSize(this.width);
+            text('🚓', this.x, this.y);
+        }
+    }
+}
+class Tree {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.velX = 0;
+        this.velY = 3;   
+        this.col = color(0, 255, 0); 
+    }
+
+    collision(thing) {
+        if (thing.x + thing.width > this.x && thing.x < this.x + this.width &&
+            thing.y + thing.height > this.y && thing.y < this.y + this.height)
+            return true;        
+        else
+            return false;            
+    }
+
+    update() {
+        this.x += this.velX;
+        this.y += runSpeed*this.velY;
+    }
+
+    show() {   
+        if (simpleGraphics) {     
+            stroke(0);
+            fill(this.col);
+            rect(this.x, this.y, 32, 32);
+        } else {
+            textSize(50);
+            text('🌳', this.x, this.y);
+        }
+    }
+}
